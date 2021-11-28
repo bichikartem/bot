a29c2dc82ef200097be4119927cd3abb01205961
bstrdn <bstrdn@gmail.com>
2021-11-28 18:04:20 +0300

    add database

--- src/main/java/ru/ortemb/contoratelegram/Bot.java
+++ src/main/java/ru/ortemb/contoratelegram/Bot.java
@@ -2,7 +2,9 @@ package ru.ortemb.contoratelegram;
 
 import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,14 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.ortemb.contoratelegram.data.repository.UserRepository;
 
 @Slf4j
 @Component
 public class Bot extends TelegramLongPollingBot {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Value("${telegram.bot.credentials.token}")
     private String TOKEN;
@@ -70,6 +76,7 @@ public class Bot extends TelegramLongPollingBot {
 
 //    @Scheduled(cron = "*/10 * * * * *")
 //    private void testSend() throws TelegramApiException {
+//        System.out.println(userRepository.findAll());
 //                SendMessage message = new SendMessage("85248441", "Hi!");
 //                execute(message);
 //    }
