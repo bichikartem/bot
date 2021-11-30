5a981cfd3c1c22b77efcb7a6c2c5800a5a422a19
bstrdn <bstrdn@gmail.com>
2021-11-30 23:00:18 +0300

    catch exception and check block

--- src/main/java/ru/ortemb/contoratelegram/controller/Bot.java
+++ src/main/java/ru/ortemb/contoratelegram/controller/Bot.java
@@ -1,7 +1,7 @@
 package ru.ortemb.contoratelegram.controller;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.ortemb.contoratelegram.data.entity.Users;
 import ru.ortemb.contoratelegram.data.mapper.UsersMapper;
 import ru.ortemb.contoratelegram.data.repository.UserRepository;
@@ -25,15 +26,20 @@ public class Bot extends TelegramLongPollingBot {
   @Value("${telegram.bot.credentials.bot-username}")
   private String BOT_USERNAME;
 
-  @SneakyThrows
   @Override
   public void onUpdateReceived(Update update) {
-
-    if (update.getMessage().hasText()) {
+    if (Objects.nonNull(update.getMessage()) && update.getMessage().hasText()) {
       if (update.getMessage().getText().equals("/start")) {
+        //todo check exists
         Users newUser = userRepository.save(usersMapper.telegramUserToEntity(update.getMessage().getFrom()));
-        execute(new SendMessage(newUser.getId(), String.format("Hi %s", newUser.getFirstName())));
+        try {
+          execute(new SendMessage(newUser.getId(), String.format("Hi %s", newUser.getFirstName())));
+        } catch (TelegramApiException e) {
+          e.printStackTrace();
+        }
       }
+    } else if (Objects.nonNull(update.getMyChatMember()) && update.getMyChatMember().getNewChatMember().getStatus().equals("kicked")) {
+      log.info("USER ID {} BLOCK YOU", update.getMyChatMember().getChat().getId());
     }
   }
 
@@ -47,19 +53,28 @@ public class Bot extends TelegramLongPollingBot {
     return TOKEN;
   }
 
-  @SneakyThrows
   @Scheduled(cron = "0 0 11 * * *", zone = "Europe/Moscow")
+//    @Scheduled(cron = "*/10 * * * * *")
   private void send() {
-    for (Users users : userRepository.findAll()) {
-      SendMessage message = new SendMessage(users.getId(), "Have a good day!");
-      execute(message);
-    }
+    userRepository.findAll().forEach(users -> {
+      try {
+        execute(new SendMessage(users.getId(), "Have a good day!"));
+      } catch (TelegramApiException e) {
+        log.info("{}. USER {}, ID: {}", e.getMessage(), users.getUserName(), users.getId());
+      }
+    });
   }
 
-//    @Scheduled(cron = "*/10 * * * * *")
-//    private void testSend() throws TelegramApiException {
-//        userRepository.findAll().stream().map(Users::getId).forEach(System.out::println);
-//        SendMessage message = new SendMessage("85248441", "Hi!");
-//        execute(message);
+//  @Transactional
+//  @Scheduled(cron = "*/10 * * * * *")
+//  void testSend() {
+////        userRepository.findAll().stream().map(Users::getId).forEach(System.out::println);
+//    SendMessage message = new SendMessage("85248441", "Hi!");
+//    try {
+//      execute(message);
+//    } catch (TelegramApiException e) {
+//      //todo get username
+//      log.info("{}. USER ID: {}", e.getMessage(), message.getChatId());
 //    }
+//  }
 }
