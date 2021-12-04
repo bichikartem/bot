c9d4f9458b235cff4623e16437e991bb12e45698
Bichik Artem <a.b.220@yandex.ru>
2021-12-04 21:40:49 +0300

    Migration, added new table - citations.

---
+++ src/main/resources/db/changelog/0.0.1/changes/03-new_table_citations.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS citations
+(
+    id varchar not null,
+    body text not null
+);
+
+COMMENT ON TABLE citations IS 'Таблица для хранения фраз и текстовых блоков';
+COMMENT ON COLUMN citations.id IS 'Идентификатор записи';
+COMMENT ON COLUMN citations.body IS 'Текст';
